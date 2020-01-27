12bb62be79641b06fb0ab392f8afc149c06301b0
Filip Fey <filipfey@gmail.com>
2020-01-27 13:23:09 +0100

    clears default react app and splits main app into header, main and footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App__header">
+        This is header
       </header>
+      <main className="App__main">
+        This is main
+      </main>
       <footer className="App__footer">
         <a href="https://icons8.com/icon/W71PIW3yxyeJ/roulette">Roulette icon by Icons8</a>
       </footer>
