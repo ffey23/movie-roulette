a788d20c9a06b7ae58011e0f64a102bc6b299b23
Filip Fey <filipfey@gmail.com>
2020-01-27 15:11:49 +0100

    converts main app component to class

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App__header">
-        This is header
-      </header>
-      <main className="App__main">
-        This is main
-      </main>
-      <footer className="App__footer">
-        <a href="https://icons8.com/icon/W71PIW3yxyeJ/roulette">Roulette icon by Icons8</a>
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+          This is header
+        </header>
+        <main className="App__main">
+        </main>
+        <footer className="App__footer">
+          <a href="https://icons8.com/icon/W71PIW3yxyeJ/roulette">Roulette icon by Icons8</a>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
