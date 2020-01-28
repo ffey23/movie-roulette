4a6254c1710ca656e384909eb752531797e45928
Filip Fey <filipfey@gmail.com>
2020-01-28 04:01:53 +0100

    creates Loader reusable component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Loader.css';
+import PropTypes from 'prop-types';
+
+class Loader extends React.Component {
+    render() {
+        const { message} = this.props;
+        return (
+            <div className="loader">
+                <div className="loader__spinner">
+                </div>
+                <div className="loader__message">
+                    { message }
+                </div>
+            </div>
+        );
+    }
+}
+
+Loader.propTypes = {
+    message: PropTypes.string,
+    show: PropTypes.bool,
+}
+export default Loader;
