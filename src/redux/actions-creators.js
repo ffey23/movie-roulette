3e3e4e465bee56fd105c642b5141ccc416666eee
Filip Fey <filipfey@gmail.com>
2020-01-28 04:00:20 +0100

    creates first store with loading reducer

---
+++ src/redux/actions-creators.js
@@ -0,0 +1,26 @@
+import {START_LOADING, FINISH_LOADING} from './actions';
+/**
+ * @param {boolean} [mainLoader=true] Determines if main loader will bi triggered 
+ * @param {string} [message="Loading..."] Loading message which we can use somewhere
+ */
+const startLoading = (mainLoader = true, message = "Loading...") => {
+    return {
+        type: START_LOADING,
+        payload: {
+            loading: true,
+            mainLoader,
+            message,
+        }
+    }
+}
+
+const finishLoading = () => {
+    return {
+        type: FINISH_LOADING,
+    }
+
+}
+export {
+    startLoading,
+    finishLoading,
+}
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import {combineReducers, createStore} from 'redux';
+import * as reducers from './reducers';
+
+const movieRoulette = combineReducers(reducers)
+
+const store = createStore(
+    movieRoulette,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+export default store;
