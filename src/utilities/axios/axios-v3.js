5917e0a09ff4c38d67e5de51626e298a6131f2dd
Filip Fey <filipfey@gmail.com>
2020-01-29 19:55:44 +0100

    configures axios for v3 and v4 movieDB API with interceptors

---
+++ src/utilities/axios/axios-v3.js
@@ -0,0 +1,20 @@
+// MovieDB API --v3-- client
+import axios from "axios";
+import { onError, onResponseSuccess } from './interceptor-callbacks';
+
+const client = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+});
+
+client.defaults.params = {
+    api_key: process.env.REACT_APP_API_BASE_URL,
+}
+
+client.interceptors.request.use(
+  config => config,
+  onError
+);
+
+client.interceptors.response.use(onResponseSuccess, onError)
+
+export default client;
