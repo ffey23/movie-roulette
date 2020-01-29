ffa097f76327e03e7f5d8c843587e3b39c8a9e6e
Filip Fey <filipfey@gmail.com>
2020-01-29 20:34:10 +0100

    adds Home component as a route in main App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
+import Home from './pages/Home';
 import MainLoader from './containers/MainLoader/MainLoader';
 import AuthButton from './containers/AuthButton/AuthButton';
 
-class App extends React.Component {
-  render() {
+const App = () => {
     return (
+      <Router>
       <div className="App">
         <header className="App__header">
             <AuthButton />
         </header>
         <main className="App__main">
+            <Switch>
+              <Route path="/" component={Home} />
+            </Switch>
           <MainLoader/>
         </main>
         <footer className="App__footer">
           <a href="https://icons8.com/icon/W71PIW3yxyeJ/roulette">Roulette icon by Icons8</a>
         </footer>
       </div>
+      </Router>
     );
-  }
+  
 }
 
 export default App;
