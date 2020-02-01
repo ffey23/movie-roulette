49d2d1eb69f03447c1fea0c90f43f56b6aa027bc
Filip Fey <filipfey@gmail.com>
2020-02-01 22:23:42 +0100

    adds normalize.css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import 'normalize.css';
 import './App.scss';
 import Home from './pages/Home';
 import Details from './pages/MovieDetails/MovieDetails';
