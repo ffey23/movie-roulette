ecf71f98a7ec0ba17523f4311e4cc662bba4fe2b
Filip Fey <filipfey@gmail.com>
2020-02-01 17:50:51 +0100

    adds PropTypes to Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = ({onClick, text}) => {
     return <button className="button" onClick={onClick}>{ text }</button> 
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button;
