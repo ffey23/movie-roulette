4f4e41d5b2cb39b1a843e14ebe3c0aee11630f73
Filip Fey <filipfey@gmail.com>
2020-02-02 16:38:34 +0100

    creates DisplayRating component

---
+++ src/components/RatingDisplay/RatingDisplay.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './RatingDisplay.scss'
+
+const RatingDisplay = ({rating}) => {
+    return (
+        <div className="rating__display">
+            <div className="rating-display__star">
+                <span className="icon-star-full"></span>
+            </div>
+            <div className="rating-display__number">
+                {rating.toFixed(1)}
+            </div>
+        </div>
+    )
+}
+
+export default RatingDisplay;
+
