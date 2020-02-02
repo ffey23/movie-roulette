3ceb13566d313e2da39994ce0aa623d1353d7d45
Filip Fey <filipfey@gmail.com>
2020-02-02 14:43:12 +0100

    styles movie item - except rating

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import './MovieItem.scss';
 
 const MovieItem = ({movie}) => {
     const [navigation, setNavigation] = useState(null);
@@ -19,12 +20,12 @@ const MovieItem = ({movie}) => {
                     alt={`Poster for movie ${movie.original_title}`}
                 />
             </div>
-            <div className="movie-item__rating">
+            {/* <div className="movie-item__rating">
                 {movie.vote_average}
-            </div>
+            </div> */}
             <div className="movie-item__info">
-                <h3 className="movie-item__heading">{movie.original_title} ({movie.release_date.split(0,4)})</h3>
-                <p className="movie-item__info">
+                <h3 className="movie-item__title" title={movie.original_title}><span>{movie.original_title} ({movie.release_date.slice(0,4)})</span></h3>
+                <p className="movie-item__other-info">
                     Language: {movie.original_language}
                 </p>
             </div>
