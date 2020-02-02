c0cbdb50d70cf10a78c0a61554994680f482030e
Filip Fey <filipfey@gmail.com>
2020-02-02 21:40:56 +0100

    removes unused variable in MovieDetails

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import api from '../../services/api';
 import Swal from 'sweetalert2';
