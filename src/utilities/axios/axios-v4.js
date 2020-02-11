f72d9a73fef90fcdbc935d644578ba19113e9035
Filip Fey <filipfey@gmail.com>
2020-02-11 21:21:37 +0100

    temporary comments axios stuff for api.middleware to work

--- src/utilities/axios/axios-v4.js
+++ src/utilities/axios/axios-v4.js
@@ -1,21 +1,23 @@
 // MovieDB API --v4-- client
 import axios from 'axios';
-import { onError, onResponseSuccess } from './interceptor-callbacks';
+import { /* onError , */ onResponseSuccess } from './interceptor-callbacks';
 
 const client = axios.create({
   baseURL: process.env.REACT_APP_API_V4_BASE_URL,
 });
 
-client.interceptors.request.use(config => {
-  return {
-    ...config,
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
-    },
-  };
-}, onError);
+client.interceptors.request.use(
+  config => {
+    return {
+      ...config,
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+      },
+    };
+  } /*onError */
+);
 
-client.interceptors.response.use(onResponseSuccess, onError);
+client.interceptors.response.use(onResponseSuccess /* onError */);
 
 export default client;
--- src/utilities/axios/interceptor-callbacks.js
+++ src/utilities/axios/interceptor-callbacks.js
@@ -1,4 +1,4 @@
-import Swal from 'sweetalert2';
+//import Swal from 'sweetalert2';
 
 const onResponseSuccess = response => {
   return response.data;
@@ -7,25 +7,26 @@ const onResponseSuccess = response => {
 /**
  * Fires Swal with error message and then re-throws error
  */
-const onError = error => {
-  /**
-   * Error message could be in error.message, error.response.data.status_message or
-   * in error.response.data.errors[0]
-   */
-  let errorMessage = error.response
-    ? error.response.data.status_message || error.response.data.errors[0]
-    : error.message;
+// const onError = error => {
+/**
+ * Error message could be in error.message, error.response.data.status_message or
+ * in error.response.data.errors[0]
+ */
+//   let errorMessage = error.response
+//     ? error.response.data.status_message || error.response.data.errors[0]
+//     : error.message;
 
-  const defaultTitle = 'Error';
-  const errorTitle = error.response
-    ? error.response.status + '!'
-    : defaultTitle;
-  Swal.fire({
-    icon: 'error',
-    title: errorTitle,
-    text: errorMessage,
-  });
-  return Promise.reject(errorMessage);
-};
+//   const defaultTitle = 'Error';
+//   const errorTitle = error.response
+//     ? error.response.status + '!'
+//     : defaultTitle;
+//   if (process.env.NODE_ENV == 'development')
+//     Swal.fire({
+//       icon: 'error',
+//       title: errorTitle,
+//       text: errorMessage,
+//     });
+//   return Promise.reject(error);
+// };
 
-export { onResponseSuccess, onError };
+export { onResponseSuccess /*onError */ };
