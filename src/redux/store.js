a1a1b5196fa02ccc552cbab20ec610b02a667269
Filip Fey <filipfey@gmail.com>
2020-02-11 21:18:12 +0100

    adds api.middleware to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import thunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
+import apiMiddleware from './middlewares/api.middleware';
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import * as reducers from './reducers';
 
@@ -7,6 +8,6 @@ const movieRoulette = combineReducers(reducers);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   movieRoulette,
-  composeEnhancers(applyMiddleware(thunkMiddleware))
+  composeEnhancers(applyMiddleware(thunk, apiMiddleware))
 );
 export default store;
