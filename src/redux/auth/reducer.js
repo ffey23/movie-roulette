bc18bd73a9982fba67611adcdc9ee81ad4259eae
Filip Fey <filipfey@gmail.com>
2020-02-12 19:41:22 +0100

    fixes loader bug in auth reducer

--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -2,6 +2,13 @@ import {
   SESSION_SUCCESS,
   DELETE_SESSION_SUCCESS,
   RETRIEVE_SESSION,
+  SESSION_FAILURE,
+  REQUEST_TOKEN_FAILURE,
+  ACCESS_TOKEN_FAILURE,
+  DELETE_SESSION_FAILURE,
+  REQUEST_TOKEN_REQUEST,
+  ACCESS_TOKEN_REQUEST,
+  DELETE_SESSION_REQUEST,
 } from './actions';
 
 const auth = (
@@ -12,20 +19,34 @@ const auth = (
   },
   action
 ) => {
-  // All failures just turns off the loading message
-  if (action.error) {
+  // Actions that turn off loading message
+  const turnOffLoading = [
+    REQUEST_TOKEN_FAILURE,
+    ACCESS_TOKEN_FAILURE,
+    SESSION_FAILURE,
+    DELETE_SESSION_FAILURE,
+  ];
+  if (turnOffLoading.includes[action.type]) {
     return {
       ...state,
       loadingMessage: null,
     };
   }
-  // All requests just turns loadingMessage with loadingMessage action in it
-  if (action.loadingMessage) {
+
+  // Actions that turn on loading message
+  // All have to have action.loadingMessage
+  const turnOnLoading = [
+    REQUEST_TOKEN_REQUEST,
+    ACCESS_TOKEN_REQUEST,
+    DELETE_SESSION_REQUEST,
+  ];
+  if (turnOnLoading.includes(action.type)) {
     return {
       ...state,
       loadingMessage: action.loadingMessage,
     };
   }
+
   switch (action.type) {
     case SESSION_SUCCESS:
       return {
