6aaae2199ce2c18e4e56e28e4bd79f2a0eaea1d2
Filip Fey <filipfey@gmail.com>
2020-02-18 18:30:40 +0100

    refactors MovieItem - styled components

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,39 +1,100 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
-import './MovieItem.scss';
 import RatingDisplay from '../RatingDisplay/RatingDisplay';
+import styled from 'styled-components';
+import { colors } from '../../styled/variables';
+import { fromMd, fromLg } from '../../styled/mixins';
 
 const MovieItem = ({ movie }) => {
   const [navigation, setNavigation] = useState(null);
   const navigateToMovie = params => {
     setNavigation(true);
   };
+
   if (navigation)
     return <Redirect to={`movie-details/${movie.id}`} push={true} />;
+
+  const Wrapper = styled.div`
+    // Needed for rating positioning
+    position: relative;
+
+    border: 2px solid ${colors.neutralDark};
+    border-radius: 5px;
+
+    // 1) Make picture grow to the title if it has to
+    ${fromMd(`
+      display: flex;
+    `)}
+
+    ${fromLg(`
+      flex-direction: column;
+      justify-content: space-between;
+    `)}
+
+    &:hover {
+      cursor: pointer;
+    }
+  `;
+  const ImageWrapper = styled.div`
+    padding: 10px;
+    // 1
+    ${fromLg(`
+      flex-grow: 1;
+  `)}
+  `;
+  const Image = styled.img`
+    width: 100%;
+    ${fromMd(`
+      width: 50px;
+    `)}
+    // 1
+    ${fromLg(`
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+    `)}
+  `;
+  const RatingDisplayWrapper = styled.div`
+    position: absolute;
+    right: -23px;
+    top: -20px;
+  `;
+  const Info = styled.div`
+    // Hides overflown text when it is flexbox item (md)
+    overflow: hidden;
+    padding: 10px;
+    ${fromLg(`
+      padding-top: 0;
+  `)}
+  `;
+  const InfoTitle = styled.h3`
+    margin-bottom: 10px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+  `;
+
+  const InfoText = styled.p``;
+
   return (
-    <div className='movie-item' onClick={navigateToMovie}>
-      <div className='movie-item__image-wrapper'>
-        <img
+    <Wrapper onClick={navigateToMovie}>
+      <ImageWrapper>
+        <Image
           src={`${process.env.REACT_APP_IMAGES_BASE_URL}w300/${movie.poster_path}`}
-          className='movie-item__image'
           alt={`Poster for movie ${movie.original_title}`}
         />
-      </div>
-      <div className='movie-item__rating'>
+      </ImageWrapper>
+      <RatingDisplayWrapper>
         <RatingDisplay rating={movie.vote_average} />
-      </div>
-      <div className='movie-item__info'>
-        <h3 className='movie-item__title' title={movie.original_title}>
-          <span>
-            {movie.original_title} ({movie.release_date.slice(0, 4)})
-          </span>
-        </h3>
-        <p className='movie-item__other-info'>
-          Language: {movie.original_language}
-        </p>
-      </div>
-    </div>
+      </RatingDisplayWrapper>
+      <Info>
+        <InfoTitle title={movie.original_title}>
+          {movie.original_title} ({movie.release_date.slice(0, 4)})
+        </InfoTitle>
+        <InfoText>Language: {movie.original_language}</InfoText>
+      </Info>
+    </Wrapper>
   );
 };
 
