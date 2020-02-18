982ec64589e5f743e9622cf2a0c4700faa043287
Filip Fey <filipfey@gmail.com>
2020-02-18 17:05:05 +0100

    refactors landing page - styled component

---
+++ src/components/Landing/Feature.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { colors } from '../../styled/variables';
+
+function Feature({ text, SvgImage }) {
+  const Wrapper = styled.div`
+    border: 2px solid ${colors.neutralDark};
+    border-radius: 10px;
+    padding: 0 10px 20px;
+    color: ${colors.textLight};
+  `;
+
+  const Text = styled.strong``;
+
+  const ImageWrapper = styled.div`
+    text-align: center;
+    padding: 20px;
+    svg {
+      width: 42px;
+      height: 42px;
+      fill: ${colors.primary};
+    }
+  `;
+  return (
+    <Wrapper>
+      <ImageWrapper>
+        <SvgImage />
+      </ImageWrapper>
+      <Text>{text}</Text>
+    </Wrapper>
+  );
+}
+
+Feature.propTypes = {
+  text: PropTypes.string.isRequired,
+  SvgImage: PropTypes.func.isRequired,
+};
+
+export default Feature;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,44 +3,49 @@ import { ReactComponent as Suggestion } from '../../assets/images/suggestion.svg
 import { ReactComponent as EmptyStar } from '../../assets/images/star-empty.svg';
 import { ReactComponent as Details } from '../../assets/images/details.svg';
 import styled from 'styled-components';
-import './Landing.scss';
+import { fromMd, fromLg } from '../../styled/mixins';
+import { colors } from '../../styled/variables';
+import Feature from './Feature';
 
-const SvgWrapper = styled.div`
+const Wrapper = styled.div`
   text-align: center;
-  padding: 20px;
-  svg {
-    width: 42px;
-    height: 42px;
-    fill: #7b181b;
-  }
+  color: ${colors.primary};
+`;
+
+const Heading = styled.h2`
+  margin-top: 10px;
+  text-align: center;
+  margin-bottom: 5px;
+`;
+
+const MainText = styled.p`
+  color: ${colors.textLight};
+  font-size: 1em;
+  font-weight: bold;
+  margin-bottom: 30px;
+`;
+
+const FeatureList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 10px;
+  ${fromMd(`grid-template-columns: 1fr 1fr 1fr;`)}
+  ${fromLg(`grid-gap: 119px;`)}
 `;
 
 const Landing = () => {
   return (
-    <div className='landing'>
-      <h2 className='landing__heading'>Don't know what to watch?</h2>
-      <p className='landing__text'>We'll make your life easier!</p>
-      <div className='landing__features'>
-        <div className='landing__feature-info'>
-          <SvgWrapper>
-            <Suggestion />
-          </SvgWrapper>
-          <strong>Get our movies suggestions</strong>
-        </div>
-        <div className='landing__feature-info'>
-          <SvgWrapper>
-            <Details />
-          </SvgWrapper>
-          <strong>Find out details about the movie</strong>
-        </div>
-        <div className='landing__feature-info'>
-          <SvgWrapper>
-            <EmptyStar />
-          </SvgWrapper>
-          <strong>Rate movies</strong>
-        </div>
-      </div>
-    </div>
+    <Wrapper>
+      <Heading className='landing__heading'>Don't know what to watch?</Heading>
+      <MainText className='landing__text'>
+        We'll make your life easier!
+      </MainText>
+      <FeatureList>
+        <Feature text='Get our movies suggestions' SvgImage={Suggestion} />
+        <Feature text='Find out details about the movie' SvgImage={Details} />
+        <Feature text='Rate movies' SvgImage={EmptyStar} />
+      </FeatureList>
+    </Wrapper>
   );
 };
 
