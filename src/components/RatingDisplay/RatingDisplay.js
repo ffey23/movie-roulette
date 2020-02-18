878d089e2fcd38a9d96d240293e7bf1175ecedc7
Filip Fey <filipfey@gmail.com>
2020-02-18 22:54:19 +0100

    refactors RatingDisplay component - styled components

--- src/components/RatingDisplay/RatingDisplay.js
+++ src/components/RatingDisplay/RatingDisplay.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import './RatingDisplay.scss';
+import styled from 'styled-components';
 
 const RatingDisplay = ({ rating }) => {
+  const Wrapper = styled.div`
+    position: relative;
+  `;
+
+  const StarWrapper = styled.div``;
+
+  const Star = styled.span`
+    font-size: 2.7em;
+  `;
+
+  const RatingNumber = styled.div`
+    position: absolute;
+    font-weight: bold;
+    top: 13px;
+    left: 10px;
+  `;
+
   return (
-    <div className='rating__display'>
-      <div className='rating-display__star'>
-        <span className='icon-star-full'></span>
-      </div>
-      <div className='rating-display__number'>{rating.toFixed(1)}</div>
-    </div>
+    <Wrapper>
+      <StarWrapper>
+        <Star className='icon-star-full'></Star>
+      </StarWrapper>
+      <RatingNumber className='rating-display__number'>
+        {rating.toFixed(1)}
+      </RatingNumber>
+    </Wrapper>
   );
 };
 
