3b5c29c55ea9bae0d80b82e157ecf62360f5bbad
Filip Fey <filipfey@gmail.com>
2020-02-18 01:40:51 +0100

    makes Apps Footer styled component

--- src/containers/App/Footer.js
+++ src/containers/App/Footer.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import { ReactComponent as MdbLogo } from '../../assets/images/movie-db-logo.svg';
-function Footer(props) {
+import { contentContainer } from './styled/mixins';
+import styled from 'styled-components';
+
+function Footer() {
+  const FooterWrapper = styled.footer``;
+
+  const Content = styled.div`
+    ${contentContainer()}
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+
+  const LinkList = styled.div`
+    margin-bottom: 12px;
+  `;
+
+  const Anchor = styled.a``;
+
+  const LogoWrapper = styled.div`
+    width: 100px;
+  `;
+
   return (
-    <footer className='App__footer'>
-      <div className='App__container App__container--footer'>
-        <div className='App__footer-href'>
-          <a href='https://icons8.com/icon/W71PIW3yxyeJ/roulette'>
+    <FooterWrapper>
+      <Content>
+        <LinkList>
+          <Anchor href='https://icons8.com/icon/W71PIW3yxyeJ/roulette'>
             Roulette icon by Icons8
-          </a>
-        </div>
-        <div className='App__movie-db-logo'>
+          </Anchor>
+        </LinkList>
+        <LogoWrapper>
           <MdbLogo />
-        </div>
-      </div>
-    </footer>
+        </LogoWrapper>
+      </Content>
+    </FooterWrapper>
   );
 }
 
