53d901b3602a89aa029f81a3f3ac6d6d34db8f67
Filip Fey <filipfey@gmail.com>
2020-02-18 17:28:15 +0100

    refactors loader - styled components

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import './Loader.scss';
 import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
 
 class Loader extends React.Component {
   render() {
     const { message } = this.props;
+
+    const Wrapper = styled.div``;
+
+    const spin = keyframes`
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+    `;
+
+    const Spinner = styled.div`
+      background: url('/logo.png') no-repeat center center;
+      background-size: contain;
+      margin: auto;
+      width: 80px;
+      height: 80px;
+      border-radius: 50%;
+      animation: ${spin} 2s linear infinite;
+      margin-bottom: 15px;
+    `;
+
+    const Message = styled.div`
+      text-align: center;
+    `;
+
     return (
-      <div className='loader'>
-        <div className='loader__spinner'></div>
-        <div className='loader__message'>{message}</div>
-      </div>
+      <Wrapper>
+        <Spinner />
+        <Message>{message}</Message>
+      </Wrapper>
     );
   }
 
