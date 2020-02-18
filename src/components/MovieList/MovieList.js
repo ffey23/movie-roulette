2b98989c7cc36179005bd9c77cd44964a4ca0077
Filip Fey <filipfey@gmail.com>
2020-02-18 22:40:28 +0100

    refactors MovieList component - styled components

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MovieItem from '../MovieItem/MovieItem';
-import './MovieList.scss';
+import styled from 'styled-components';
+import { fromLg } from '../../styled/mixins';
 
 const MovieList = ({ movies }) => {
+  const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: minmax(0, 1fr);
+    grid-gap: 20px;
+    ${fromLg(`
+        grid-gap: 40px;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+    `)}
+  `;
   const movieItems = movies.map(m => <MovieItem movie={m} key={m.id} />);
-  return <div className='movie-list'>{movieItems}</div>;
+  return <Wrapper>{movieItems}</Wrapper>;
 };
 
 MovieList.propTypes = {
