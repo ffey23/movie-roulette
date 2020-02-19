f3ad681ffd48de31ba0d4e9e66402b556b527338
Filip Fey <filipfey@gmail.com>
2020-02-19 15:00:50 +0100

    refactors MainLoader - styled components

--- src/containers/MainLoader/MainLoader.js
+++ src/containers/MainLoader/MainLoader.js
@@ -2,16 +2,38 @@ import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Loader from '@/components/Loader/Loader';
+import styled from 'styled-components';
 
 // Needed for conditionally show component
 class MainLoader extends Component {
   render() {
     const { message } = this.props;
     if (message == null) return null;
+
+    const Wrapper = styled.div`
+      /**
+       * Makes loader-wrapper cover whole screen
+       */
+      position: fixed;
+      z-index: 10000;
+      top: 0;
+      width: 100%;
+      height: 100vh;
+      /**
+       * Centers loader
+       */
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      background-color: rgba(0, 0, 0, 0.6);
+      cursor: wait;
+    `;
+
     return (
-      <div className='loader-wrapper'>
+      <Wrapper>
         <Loader blockScroll={true} message={message} />
-      </div>
+      </Wrapper>
     );
   }
 }
