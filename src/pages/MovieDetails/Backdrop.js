08bc771a8d570e57d638fa889e27290c736eef3c
Filip Fey <filipfey@gmail.com>
2020-02-24 23:25:30 +0100

    prevents reflow on MovieDetails image load

--- src/pages/MovieDetails/Backdrop.js
+++ src/pages/MovieDetails/Backdrop.js
@@ -2,18 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { fromMd } from '@/styled/mixins';
+import { colors } from '@/styled/variables';
 
 function Backdrop({ src, alt, text }) {
   const Wrapper = styled.div`
     position: relative;
-  `;
-
-  const Image = styled.img`
-    width: 100%;
     margin-bottom: 12px;
   `;
 
+  const ImageWrapper = styled.div`
+    padding-bottom: 56.19916%;
+    position: relative;
+  `;
+  const Image = styled.img`
+    width: 100%;
+    position: absolute;
+  `;
+
   const Overview = styled.div`
+    background-color: white;
+    border-bottom: 2px solid ${colors.neutralDark};
+    border-left: 2px solid ${colors.neutralDark};
+    border-right: 2px solid ${colors.neutralDark};
+    border-radius: 0 0 3px 3px;
+    padding: 6px 2px;
     margin-bottom: 12px;
     ${fromMd(`
       position: absolute;
@@ -21,13 +33,16 @@ function Backdrop({ src, alt, text }) {
       bottom: 0;
       width: 400px;
       padding: 10px;
-      background-color: white;
+      border: none;
+      margin-bottom: 0;
     `)}
   `;
 
   return (
     <Wrapper>
-      <Image src={src} alt={alt} />
+      <ImageWrapper>
+        <Image src={src} alt={alt} />
+      </ImageWrapper>
       <Overview className='movie-details__overview'>{text}</Overview>
     </Wrapper>
   );
