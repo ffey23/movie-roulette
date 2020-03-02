bd5fdd0cd00c1a5134f968a3198c42e355110dc6
Filip Fey <filipfey@gmail.com>
2020-03-02 14:14:46 +0100

    adds comment to MovieList explaining why we must have css module file

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MovieItem from './MovieItem';
 
+// We can't use styled components here because MovieItem would rerender when it shouldn't (on load more)
 import styles from './MovieList.module.scss';
 
 const MovieList = ({ movies }) => {
